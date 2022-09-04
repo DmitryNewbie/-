c66c29a87f8f406a916b4bbdd811cabe25fa2d40
Dmitry <zimzulka1@gmail.com>
2022-09-04 22:26:03 +0400

    Итог

---
+++ 1задание/Program.cs
@@ -0,0 +1,18 @@
+﻿int a,b;
+Console.WriteLine("Введите два числа");
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+
+{
+    Console.WriteLine("Первое число больше");
+    Console.WriteLine("Второе число меньше");
+}
+else if(b>a)
+{
+    Console.WriteLine("Второе число больше");
+    Console.WriteLine("Первое число меньше");
+}
+
